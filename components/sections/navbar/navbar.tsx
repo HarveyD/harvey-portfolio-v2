b174f1d71d19cc34d186cd8dbb07ff8bed701c02
Harvey <harveydelaney@yahoo.com.au>
2020-05-17 15:14:29 +1000

    feat: add smooth scrolling to internal links from navbar

--- components/sections/navbar/navbar.tsx
+++ components/sections/navbar/navbar.tsx
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import DarkModeToggle from "./dark-mode-toggle/dark-mode-toggle";
 
 import "./navbar.scss";
 
-const links = [
-  { description: "About", link: "/#about" },
-  { description: "Experience", link: "/#experience" },
-  { description: "Education", link: "/#education" },
-  { description: "Projects", link: "/#projects" },
-  { description: "Skills", link: "/#skills" },
-  { description: "Contact", link: "/#contact" },
-  { description: "Blog", link: "https://blog.harveydelaney.com" },
+interface NavItem {
+  description: string;
+  link: string;
+  isExternal?: boolean;
+}
+
+const navItems: NavItem[] = [
+  { description: "About", link: "#about" },
+  { description: "Experience", link: "#experience" },
+  { description: "Education", link: "#education" },
+  { description: "Projects", link: "#projects" },
+  { description: "Skills", link: "#skills" },
+  { description: "Contact", link: "#contact" },
+  {
+    description: "Blog",
+    link: "https://blog.harveydelaney.com",
+    isExternal: true,
+  },
 ];
 
 const Navbar: React.FC = () => {
+  const scrollIntoView = useCallback((e, item) => {
+    if (item.isExternal) {
+      return;
+    }
+
+    e.preventDefault();
+
+    document.querySelector(item.link).scrollIntoView({
+      behavior: "smooth",
+    });
+  }, []);
+
   return (
     <nav className="navbar-container">
       <div className="navbar-content">
         <div className="nav-items-container">
-          {links.map(({ description, link }) => (
-            <a className="nav-item" href={link}>
-              {description}
+          {navItems.map((item) => (
+            <a
+              onClick={(e) => scrollIntoView(e, item)}
+              className="nav-item"
+              href={item.link}
+            >
+              {item.description}
             </a>
           ))}
         </div>
